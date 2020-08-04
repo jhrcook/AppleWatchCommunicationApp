9f5cbd2c9582b2916bcfa8e430e91bc9b173d343
Josh Cook <joshuacook0023@gmail.com>
2020-08-04 06:20:32 -0400

    activate WatchOS extension

--- AppleWatchCommunicationApp/utilities.swift
+++ AppleWatchCommunicationApp/utilities.swift
@@ -12,4 +12,3 @@ func getDocumentsDirectory() -> URL {
     let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
     return paths[0]
 }
-
---
+++ WatchAppleWatchCommunicationApp Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchAppleWatchCommunicationApp Extension
+//
+//  Created by Joshua on 8/4/20.
+//  Copyright © 2020 Joshua Cook. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
